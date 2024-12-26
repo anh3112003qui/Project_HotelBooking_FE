8f6558d7cb53699cd6d34f8abc060a77456ebc4b
anh3112003qui <anh.qui0311@gmail.com>
2024-12-27 02:17:35 +0700

    fix loi icon carousel

--- QuiLe-Hotel/src/components/home/Home.jsx
+++ QuiLe-Hotel/src/components/home/Home.jsx
@@ -1,36 +1,46 @@
-import React, { useContext } from "react"
+import React, { useState, useEffect } from "react"
 import MainHeader from "../layout/MainHeader"
 import HotelService from "../common/HotelService"
 import Parallax from "../common/Parallax"
 import RoomCarousel from "../common/RoomCarousel"
 import RoomSearch from "../common/RoomSearch"
 import { useLocation } from "react-router-dom"
-import { useAuth } from "../auth/AuthProvider"
-
 
 const Home = () => {
-	const location = useLocation()
+    const location = useLocation()
+    const [showMessage, setShowMessage] = useState(false)
+    const message = location.state?.message
+    const currentUser = localStorage.getItem("userId")
 
-	const message = location.state && location.state.message
-	const currentUser = localStorage.getItem("userId")
-	return (
-		<section>
-			{message && <p className="text-warning px-5">{message}</p>}
-			{currentUser && (
-				<h6 className="text-success text-center"> Bạn đã đăng nhập với tài khoản {currentUser}</h6>
-			)}
-			<MainHeader />
-			<div className="container">
-				<RoomSearch/>
+    useEffect(() => {
+        if (message) {
+            setShowMessage(true)
+            const timer = setTimeout(() => {
+                setShowMessage(false)
+            }, 5000) // 5 giây
+
+            return () => clearTimeout(timer) // Dọn dẹp bộ đếm thời gian
+        }
+    }, [message])
+
+    return (
+        <section>
+            {showMessage && <p className="text-warning px-5">{message}</p>}
+            {currentUser && (
+                <h6 className="text-success text-center"> Bạn đã đăng nhập với tài khoản {currentUser}</h6>
+            )}
+            <MainHeader />
+            <div className="container">
+                <RoomSearch />
                 <RoomCarousel />
-				<Parallax />
-				<RoomCarousel />
-				<HotelService />
-				<Parallax />
-				<RoomCarousel />
-			</div>
-		</section>
-	)
+                <Parallax />
+                <RoomCarousel />
+                <HotelService />
+                <Parallax />
+                <RoomCarousel />
+            </div>
+        </section>
+    )
 }
 
 export default Home
